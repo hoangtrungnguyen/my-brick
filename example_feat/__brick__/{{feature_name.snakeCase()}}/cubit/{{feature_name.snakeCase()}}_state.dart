bbf54c515ac7fb90ab3a5695a60b5cbe836bb78e
nguyen <nguyen.hoang@ohmidasvn.com>
2024-02-01 15:59:38 +0700

    update cubit state class

--- example_feat/__brick__/{{feature_name.snakeCase()}}/cubit/{{feature_name.snakeCase()}}_state.dart
+++ example_feat/__brick__/{{feature_name.snakeCase()}}/cubit/{{feature_name.snakeCase()}}_state.dart
@@ -3,5 +3,9 @@ part of '{{feature_name.snakeCase()}}_cubit.dart';
 @freezed
 class {{feature_name.pascalCase()}}State with _${{feature_name.pascalCase()}}State {
 
+  factory {{feature_name.pascalCase()}}State({
+    @Default('') String name
+  }) = Initial;
 
+  {{feature_name.pascalCase()}}State._();
 }
--- example_feat/__brick__/{{feature_name.snakeCase()}}/{{feature_name.snakeCase()}}.dart
+++ example_feat/__brick__/{{feature_name.snakeCase()}}/{{feature_name.snakeCase()}}.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubit/{{feature_name.snakeCase()}}_cubit.dart';
+
 
 class {{feature_name.pascalCase()}} extends StatelessWidget {
     const {{feature_name.pascalCase()}}({super.key});
 
     @override
     Widget build(BuildContext context) {
-        return  BlocProvider<EditorCubit>(
+        return  BlocProvider<{{feature_name.pascalCase()}}Cubit>(
             create: (BuildContext context){
                 return {{feature_name.pascalCase()}}Cubit();
             },
