b50a02291b1d02e9909535890b9293f56de96786
nguyen.hoang <toai.levan@ohmidasvn.com>
2025-06-10 14:51:52 +0700

    model version 0.1.0+3

--- bricks/model/__brick__/{{name.snakeCase()}}_model.dart
+++ bricks/model/__brick__/{{name.snakeCase()}}_model.dart
@@ -1,14 +1,14 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part '{{name.snakeCase()}}.freezed.dart';
-part '{{name.snakeCase()}}.g.dart';
+part '{{name.snakeCase()}}_model.freezed.dart';
+part '{{name.snakeCase()}}_model.g.dart';
 
 @freezed
-abstract class {{#pascalCase}}{{name}}{{/pascalCase}} with _${{#pascalCase}}{{name}}{{/pascalCase}} {
-  factory {{#pascalCase}}{{name}}{{/pascalCase}}({
+abstract class {{#pascalCase}}{{name}}{{/pascalCase}}Model with _${{#pascalCase}}{{name}}{{/pascalCase}}Model {
+  factory {{#pascalCase}}{{name}}{{/pascalCase}}Model({
     required int id,
-  }) = _{{#pascalCase}}{{name}}{{/pascalCase}};
+  }) = _{{#pascalCase}}{{name}}{{/pascalCase}}Model;
 
-  factory {{#pascalCase}}{{name}}{{/pascalCase}}.fromJson(Map<String, dynamic> json) =>
-      _${{#pascalCase}}{{name}}{{/pascalCase}}FromJson(json);
+  factory {{#pascalCase}}{{name}}{{/pascalCase}}Model.fromJson(Map<String, dynamic> json) =>
+      _${{#pascalCase}}{{name}}{{/pascalCase}}ModelFromJson(json);
 }
