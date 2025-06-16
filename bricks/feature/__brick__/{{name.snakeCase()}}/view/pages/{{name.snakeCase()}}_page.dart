d79e218cc8b60685172072f26ebcbb9ad978d53d
nguyen.hoang <toai.levan@ohmidasvn.com>
2025-06-16 15:54:30 +0700

    update brick feature

--- bricks/feature/__brick__/{{name.snakeCase()}}/view/pages/{{name.snakeCase()}}_page.dart
+++ bricks/feature/__brick__/{{name.snakeCase()}}/view/pages/{{name.snakeCase()}}_page.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../bloc/{{name.snakeCase()}}/{{name.snakeCase()}}.dart';
-
+import '../bloc/bloc.dart';
 
 class {{name.pascalCase()}}Page extends StatelessWidget {
 
-      static const String routePath = '{{#pathCase}}{{name}}{{/pathCase}}';
+      static const String routePath = '/{{#snakeCase}}{{name}}{{/snakeCase}}';
   static const String routeName = '{{#pascalCase}}{{name.pascalCase()}}{{/pascalCase}}';
 
- const {{#pascalCase}}{{name.pascalCase()}}{{/pascalCase}}: ({super.key});
+ const {{#pascalCase}}{{name.pascalCase()}}{{/pascalCase}}Page({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-            appBar: AppBar(title: const Text("App Bar"),
-            centerTitle: true,
-            body: Center(
-                child: Text("Developing")
-            )
-        );
+   return Scaffold(
+      appBar: AppBar(title: const Text("App Bar"), centerTitle: true),
+      body: Center(child: Text("Developing")),
+    );
   }
 }
